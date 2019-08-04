0441e588b49be4bb55c8e5b866e80dc65ca6b6e9
sarbesh <sarbeshsarkarsks@gmail.com>
2019-08-04 19:50:44 +0530

    toogle fix partly

--- src/main/resources/static/js/myJs.js
+++ src/main/resources/static/js/myJs.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
-	alert("loaded");
+	// alert("loaded");
 	
 	var xhr = new XMLHttpRequest();
-	var singupurl = "http://localhost:8080/api/register";
+	var singupUrl = "http://localhost:8080/api/register";
+	var loginUrl = "http://localhost:8080/api/login";
+	var searchUrl = "http://localhost:8080/api/search";
+	var detailsUrl = "http://localhost:8080/api/employee/{id}";
 
 	$("#login-show").click(function() {
 		$("#signupDiv").toggle();
 		$("#loginDiv").toggle();
-		alert("login-show");
+		// alert("login-show");
 	});
 
 	$("#create-show").click(function() {
 		$("#loginDiv").toggle();
 		$("#signupDiv").toggle();
-		alert("signup-show");
+		// alert("signup-show");
 	});
 });
